63097b9fe0629674b1477a1159b86b34eb54d93a
Sanat Shah <sunnyshah711@gmail.com>
2016-02-02 16:35:19 -0500

    set up .c file

---
+++ Thread_Library/ult/mypthread.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<string.h>    
+
+
+
+
+
+int mypthread_create(mypthread_t *thread, const mypthread_attr_t *attr,
+                        void *(*start_routine) (void *), void *arg){
+
+
+
+
+
+
+
+}
+
+
+int mypthread_create(mypthread_t *thread, const mypthread_attr_t *attr,
+                        void *(*start_routine) (void *), void *arg){
+
+
+
+
+
+
+
+
+
+}
+
+
+int mypthread_yield(void){
+
+
+
+
+
+
+
+}
+
+
+int mypthread_yield(void){
+
+
+
+
+
+
+
+
+
+
+}
